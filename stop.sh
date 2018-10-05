592aa6e5263f3bbaf1dbf15c8088247c831ddd79
mark-cohen <veberfetzel@gmail.com>
2018-10-04 23:46:54 -0700

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+
 ################################################################################
 #
-# Scrip Created by http://CryptoLions.io
-# https://github.com/CryptoLions/EOS-Jungle-Testnet
+# Script created by http://CryptoLions.io
+# For EOS mainnet
+# https://github.com/CryptoLions/EOS-MainNet
+#
+# Modified for Telos Testnet
+# last edited by Telos Vancouver 20180921
 #
 ###############################################################################
 
-DIR="/opt/JungleTestnet"
+DIR="/opt/TelosTestnet/Data"
 
 
-    if [ -f $DIR"/nodeos.pid" ]; then
-	pid=`cat $DIR"/nodeos.pid"`
-	echo $pid
-	kill $pid
-	rm -r $DIR"/nodeos.pid"
+ if [ -f $DIR"/nodeos.pid" ]; then
+        pid=`cat $DIR"/nodeos.pid"`
+        echo $pid
+        kill $pid
+        rm -r $DIR"/nodeos.pid"
 
-	echo -ne "Stoping Nodeos"
+        echo -ne "Stopping Nodeos"
 
         while true; do
             [ ! -d "/proc/$pid/fd" ] && break
