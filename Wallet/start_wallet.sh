f0d663d673f5066f0aea7f93ae16990d1afc3e89
mark-cohen <veberfetzel@gmail.com>
2018-10-04 23:51:42 -0700

    Update start_wallet.sh

--- Wallet/start_wallet.sh
+++ Wallet/start_wallet.sh
@@ -2,16 +2,20 @@
 
 ################################################################################
 #
-# Scrip Created by http://CryptoLions.io
-# https://github.com/CryptoLions/EOS-Jungle-Testnet/
+# Script created by http://CryptoLions.io
+# For EOS mainnet
+# https://github.com/CryptoLions/EOS-MainNet
+#
+# Modified for Telos Testnet
+# last edited by Telos Vancouver 20180921
 #
 ###############################################################################
 
-NODEOSBINDIR="/home/eos-v1.0/eos/build/programs"
-DATADIR="/opt/JungleTestnet/Wallet"
+NODEOSBINDIR="/opt/TelosTestnet/telos/build/programs"
+DATADIR="/opt/TelosTestnet/Data/telos-wallet"
 WALLET_HOST="127.0.0.1"
 WALLET_POSRT="3000"
 
 
 $DATADIR/stop_wallet.sh
-$NODEOSBINDIR/keosd/keosd --config-dir $DATADIR --wallet-dir $DATADIR --http-server-address $WALLET_HOST:$WALLET_POSRT $@ & echo $! > $DATADIR/wallet.pid
+$NODEOSBINDIR/tkeosd/tkeosd --config-dir $DATADIR --wallet-dir $DATADIR --http-server-address $WALLET_HOST:$WALLET_POSRT $@ & echo $! > $DATADIR/wallet.pid
